1d41ef6f32a116c51eff2350fd522d84e5f01c33
ajinath72 <ajinathpawar72@gmail.com>
2023-04-14 12:26:29 +0530

    ExpenseDetails

--- src/Components/Expense.jsx
+++ src/Components/Expense.jsx
@@ -1,17 +1,18 @@
 import './Expenses.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 
-
-function Expense(props){
-    return (  
-      <div className='expense-item'>  
-        <div>{props.date.toISOString()}</div>
-        <div className='expense-item__description'>
-          <h2>{props.title}</h2>
-          <div className='expense-item__price'>${props.amount}</div>
-        </div>
-        <div>{props.LocationOfExpenditure}</div>
+function Expense(props) {
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={props.date}/>
+      <div className='expense-item__description'>
+        <h2 className='expense-item__title'>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-    );
-  }
-  
-  export default Expense;
+      <ExpenseDetails LocationOfExpenditure={props.LocationOfExpenditure} />
+    </div>
+  );
+}
+
+export default Expense;
